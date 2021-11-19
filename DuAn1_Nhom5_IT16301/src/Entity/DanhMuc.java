764dc517878c4547cdd3d66e71c705abfe9a2d49
hoaquangsy <85879639+SyHQ29@users.noreply.github.com>
2021-11-19 11:36:36 +0700

    code moi

---
+++ DuAn1_Nhom5_IT16301/src/Entity/DanhMuc.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class DanhMuc {
+    private String maDM;
+    private String tenDm;
+    private String moTa;
+
+    public DanhMuc() {
+    }
+
+    public DanhMuc(String maDM, String tenDm, String moTa) {
+        this.maDM = maDM;
+        this.tenDm = tenDm;
+        this.moTa = moTa;
+    }
+
+    public String getMaDM() {
+        return maDM;
+    }
+
+    public void setMaDM(String maDM) {
+        this.maDM = maDM;
+    }
+
+    public String getTenDm() {
+        return tenDm;
+    }
+
+    public void setTenDm(String tenDm) {
+        this.tenDm = tenDm;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+    
+}
